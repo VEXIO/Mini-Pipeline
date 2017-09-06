e1b5f9e7057d3c423328a1ee8d9f3c653c3c3ee7
Haotian <liuhaotian.cn@gmail.com>
2017-09-06 16:03:15 +0800

    Use multi-port mem core

--- Mini-Pipeline.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0.veo
+++ Mini-Pipeline.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0.veo
@@ -56,11 +56,15 @@
 //----------- Begin Cut here for INSTANTIATION Template ---// INST_TAG
 blk_mem_gen_0 your_instance_name (
   .clka(clka),    // input wire clka
-  .rsta(rsta),    // input wire rsta
   .wea(wea),      // input wire [0 : 0] wea
-  .addra(addra),  // input wire [9 : 0] addra
-  .dina(dina),    // input wire [15 : 0] dina
-  .douta(douta)  // output wire [15 : 0] douta
+  .addra(addra),  // input wire [18 : 0] addra
+  .dina(dina),    // input wire [11 : 0] dina
+  .douta(douta),  // output wire [11 : 0] douta
+  .clkb(clkb),    // input wire clkb
+  .web(web),      // input wire [0 : 0] web
+  .addrb(addrb),  // input wire [18 : 0] addrb
+  .dinb(dinb),    // input wire [11 : 0] dinb
+  .doutb(doutb)  // output wire [11 : 0] doutb
 );
 // INST_TAG_END ------ End INSTANTIATION Template ---------
 
--- Mini-Pipeline.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
+++ Mini-Pipeline.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2016.2 (win64) Build 1577090 Thu Jun  2 16:32:40 MDT 2016
-// Date        : Wed Sep 06 10:42:55 2017
+// Date        : Wed Sep 06 14:52:42 2017
 // Host        : DESKTOP-DM3G5QT running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
-//               z:/Mini-Pipeline/Mini-Pipeline.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
+//               Z:/Mini-Pipeline/Mini-Pipeline.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
 // Design      : blk_mem_gen_0
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7k325tffg676-2L
@@ -14,12 +14,16 @@
 // The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
 // Please paste the declaration into a Verilog source file or add the file as an additional source.
 (* x_core_info = "blk_mem_gen_v8_3_3,Vivado 2016.2" *)
-module blk_mem_gen_0(clka, rsta, wea, addra, dina, douta)
-/* synthesis syn_black_box black_box_pad_pin="clka,rsta,wea[0:0],addra[9:0],dina[15:0],douta[15:0]" */;
+module blk_mem_gen_0(clka, wea, addra, dina, douta, clkb, web, addrb, dinb, doutb)
+/* synthesis syn_black_box black_box_pad_pin="clka,wea[0:0],addra[18:0],dina[11:0],douta[11:0],clkb,web[0:0],addrb[18:0],dinb[11:0],doutb[11:0]" */;
   input clka;
-  input rsta;
   input [0:0]wea;
-  input [9:0]addra;
-  input [15:0]dina;
-  output [15:0]douta;
+  input [18:0]addra;
+  input [11:0]dina;
+  output [11:0]douta;
+  input clkb;
+  input [0:0]web;
+  input [18:0]addrb;
+  input [11:0]dinb;
+  output [11:0]doutb;
 endmodule
